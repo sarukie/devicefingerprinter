dc1b73f1718ebad16cdd56241ef73d18ed44dc6b
Kurt Newcomb <kurtn@accretivetg.com>
2016-04-15 14:15:55 -0700

    Adding module for sqlite db

--- config/module.py
+++ config/module.py
@@ -6,10 +6,20 @@ table_one = 'fingerprintid'
 table_two = 'deviceid'
 table_three = 'userid'
 table_four = 'ipaddress' 
+column_user = 'INTEGER'
+colump_ip = 'TEXT'
 
 # Connect
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor() 
+
+#create user column
+c.execute('CREATE TABLE {tn} ({nf} {ft})'\
+        .format(tn=table_three, nf=new_field, ft=column_user))
+
+#create ipaddress column
+c.execute('CREATE TABLE {tn} ({nf} {ft})'\
+        .format(tn=table_four, nf=new_field, ft=column_ip))
 
 # Close Connection
 conn.commit()
