ca207220011b8b66c5349e064f6f3fa24e89513f
Kurt Newcomb <kurtn@accretivetg.com>
2016-04-15 15:33:28 -0700

    updated sqlite module

--- config/module.py
+++ config/module.py
@@ -1,14 +1,5 @@
 import sqlite3
 
-# Vars
-sqlite_file = 'data.db'
-table_one = 'fingerprintid'
-table_two = 'deviceid'
-table_three = 'userid'
-table_four = 'ipaddress'
-column_user = 'INTEGER'
-colump_ip = 'TEXT'
-
 class Module:
 
     def save(userid, deviceid, ipaddress):
